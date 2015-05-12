db3e1f5fc150b9c36ade2d68231abcbdb01690e2
Elisa <Elisa@192.168.1.10>
2015-05-12 13:54:42 +0200

    12/02/2015

---
+++ Bataille_Navale/src/fr/iutvalence/blancarayt/bataillenavale/UnvalidDirection.java
@@ -0,0 +1,9 @@
+package fr.iutvalence.blancarayt.bataillenavale;
+
+public class UnvalidDirection extends Exception
+{
+	/* TODO This constructor should not be used. Prefer a message or a throwable. */
+    public UnvalidDirection(String message) {
+    	super(message);
+    }
+}
