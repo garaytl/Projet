922875fda7100cb274e3fafe4e7415897d2bfd04
blancare <eli-072009@hotmail.fr>
2015-05-22 16:46:16 +0200

    22/05/2015

--- Battleship/src/ihm/Affichage.java
+++ Battleship/src/ihm/Affichage.java
@@ -1,7 +1,15 @@
 package ihm;
+/**
+ * Affichage
+ * @author Elisa
+ *
+ */
 public class Affichage
 {
-
+	/**
+	 * Main.
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		Fenetre fenetre = new Fenetre();
